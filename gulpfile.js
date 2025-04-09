384cdf3087944e5ca32720e0899719ef7255e25b
John Doe <johndoe@example.com>
2025-04-09 10:09:29 +0300

    Додано main.js, виконано мініфікацію CSS та JS

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,35 @@
 const gulp = require('gulp');
+const cleanCSS = require('gulp-clean-css'); // Підключаємо clean-css
+const uglify = require('gulp-uglify');      // Підключаємо uglify
 
-// Завдання для копіювання файлів з app в public
-exports.cloneFiles = () => {
-  return gulp.src('app/**/*') // всі файли з папки app
-    .pipe(gulp.dest('public')); // копіюємо в папку public
+// Завдання: копіювання всіх файлів з папки app
+exports.cloneAll = async () => {
+  return gulp.src('app/**/*')
+    .pipe(gulp.dest('public'));
+}
+
+// Завдання: копіювання тільки CSS файлів
+exports.cloneCSS = async () => {
+  return gulp.src('app/**/*.css')
+    .pipe(gulp.dest('public'));
+}
+
+// Завдання: копіювання тільки JS файлів
+exports.cloneJS = async () => {
+  return gulp.src('app/**/*.js')
+    .pipe(gulp.dest('public'));
+}
+
+// Завдання: мініфікація CSS файлів
+exports.minifyCSS = async () => {
+  return gulp.src('app/**/*.css')
+    .pipe(cleanCSS()) // Мінімізація CSS
+    .pipe(gulp.dest('public'));
+}
+
+// Завдання: мініфікація JS файлів
+exports.minifyJS = async () => {
+  return gulp.src('app/**/*.js')
+    .pipe(uglify()) // Мінімізація JS
+    .pipe(gulp.dest('public'));
 }
